164abe003f2f96be94b3b97361cda7a789adc845
Tulipreactjsmain <rdhnft@gmail.com>
2024-04-04 12:03:30 +0100

    init quizzler game

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'quiz_brain.dart';
+import 'package:flutter/material.dart';
+
+QuizBrain quizBrain = QuizBrain();
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+  int questionNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(questionNumber),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.amber),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                bool correctAnswer =
+                    quizBrain.getQuestionAnswer(questionNumber);
+                correctAnswer ? print("you're right") : print("you're wrong");
+                setState(() {
+                  questionNumber++;
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              // color: Colors.red,
+              style: TextButton.styleFrom(backgroundColor: Colors.red.shade600),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                bool correctAnswer =
+                    quizBrain.getQuestionAnswer(questionNumber);
+                correctAnswer ? print("you're right") : print("you're wrong");
+                setState(() {
+                  questionNumber++;
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
---
+++ lib/question.dart
@@ -0,0 +1,10 @@
+class Question {
+  late String questionText;
+  late bool questionAnswer;
+
+  // Constructor
+  Question(String q, bool a) {
+    this.questionText = q;
+    this.questionAnswer = a;
+  }
+}
